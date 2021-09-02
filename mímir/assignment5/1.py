52d681030f8f19e5f475fe46d94d0e3679af38bf
aronorn02 <aronoa21@ru.is>
2021-09-02 14:15:39 +0000

    klárað nr 1 :)

---
+++ mímir/assignment5/1.py
@@ -0,0 +1,21 @@
+
+
+num_int = int(input("Input a number: "))    # Do not change this line
+
+
+# Fill in the missing code
+              
+max_int = num_int
+
+new_num = 0
+
+
+while new_num >= 0:
+    new_num = int(input("Input a number: "))
+    if new_num > max_int:
+        max_int = new_num
+    num_int = new_num
+
+
+
+print("The maximum is", max_int)    # Do not change this line
